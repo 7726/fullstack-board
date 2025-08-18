1ba77c545fd949ccfeea4f57fd0401aa148cce36
7726 <bingo.devops@gmail.com>
2025-08-18 15:54:18 +0900

    first commit

---
+++ src/main/java/com/example/fullstack_board/FullstackBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.fullstack_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackBoardApplication.class, args);
+	}
+
+}
